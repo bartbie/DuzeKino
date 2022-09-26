b2227d20d297dcc81a2dc55ece8c56b7534d88af
BalazsGitHub <balazs.dombovari@gmail.com>
2022-09-26 18:06:05 +0200

    added Enum and converter for checking the access level of the user

---
+++ src/main/java/org/duze/duzekino/model/Permission.java
@@ -0,0 +1,20 @@
+package org.duze.duzekino.model;
+
+/**
+ * enum representation
+ */
+public enum Permission {
+    ADMIN(100),
+    BASIC(0);
+
+    private final Integer level;
+
+    Permission(Integer level) {
+        this.level = level;
+    }
+
+
+    public Integer getLevel() {
+        return level;
+    }
+}
