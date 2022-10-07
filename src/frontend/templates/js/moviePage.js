4be6a0cc56afd65930ae58c01f28e6bc1e0291bf
Martinnh1993 <90694207+Martinnh1993@users.noreply.github.com>
2022-10-07 15:34:27 +0200

    Movie card css and html

---
+++ src/frontend/templates/js/moviePage.js
@@ -0,0 +1,9 @@
+newMoviePopup_btn.addEventListener('click', openPopup)
+
+saveMoviePopup_btn.addEventListener('click', saveMovie)
+saveMoviePopup_btn.addEventListener('click', addMovieCardFromDB)
+saveMoviePopup_btn.addEventListener('click', closePopup)
+
+editMoviePopup_btn.addEventListener('click', openPopup)
+
+input_image.addEventListener("change", setCoverImage)
